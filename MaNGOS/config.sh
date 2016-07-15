31afbda11e673e285fffd9ba50c53ff13bb912f3
Deyan Dobromirov <dvd_video@abv.bg>
2016-07-15 16:24:44 +0300

    Updated: MaNGOS/config.sh with database installation for WoTLK

--- MaNGOS/config.sh
+++ MaNGOS/config.sh
@@ -2,7 +2,6 @@
 
 # Based on https://github.com/cmangos/issues/wiki/Installation-Instructions
 
-
 action="$1"
 
 bool=""
@@ -73,10 +72,7 @@ case "$action" in
       sudo apt-get install libboost-all-dev
     fi
 
-    # Set the proxy if any
-    echo "Are you using a proxy [no or <proxy:port>]  ?"
-    read -r proxysv
-
+    read -p "Are you using a proxy [no or <proxy:port>] ? " proxysv
     if test "$proxysv" == "no"
     then
       sudo git config --global -l
@@ -86,28 +82,32 @@ case "$action" in
       echo "Proxy set to [$proxysv] !"
     fi
 
-    if [ 1 -eq 0 ]; then #comment
-
-    case "$idtitle" in
-    "0")
-      git clone https://github.com/cmangos/mangos-classic.git $scriptpath/mangos
-      git clone https://github.com/ACID-Scripts/Classic.git $scriptpath/acid
-    ;;
-    "1")
-      git clone https://github.com/cmangos/mangos-tbc.git $scriptpath/mangos
-      git clone https://github.com/ACID-Scripts/TBC.git $scriptpath/acid
-    ;;
-    "2")
-      git clone https://github.com/cmangos/mangos-wotlk.git $scriptpath/mangos
-      git clone https://github.com/ACID-Scripts/WOTLK.git $scriptpath/acid
-      git clone https://github.com/unified-db/Database.git $scriptpath/unifieddb
-    ;;
-    "3")
-      git clone https://github.com/cmangos/mangos-cata.git $scriptpath/mangos
-      git clone https://github.com/ACID-Scripts/CATA.git $scriptpath/acid
-      git clone https://github.com/UDB-434/Database.git $scriptpath/unifieddb
-    ;;
-    esac
+    read -p "Do you wish to download the sources now [y or n] ? " bool
+    if test "$bool" == "y"
+    then
+      case "$idtitle" in
+      "0")
+        git clone https://github.com/cmangos/mangos-classic.git $scriptpath/mangos
+        git clone https://github.com/ACID-Scripts/Classic.git $scriptpath/acid
+        git clone https://github.com/classicdb/database.git $scriptpath/db
+      ;;
+      "1")
+        git clone https://github.com/cmangos/mangos-tbc.git $scriptpath/mangos
+        git clone https://github.com/ACID-Scripts/TBC.git $scriptpath/acid
+        git clone https://github.com/TBC-DB/Database.git $scriptpath/db
+      ;;
+      "2")
+        git clone https://github.com/cmangos/mangos-wotlk.git $scriptpath/mangos
+        git clone https://github.com/ACID-Scripts/WOTLK.git $scriptpath/acid
+        git clone https://github.com/unified-db/Database.git $scriptpath/db
+      ;;
+      "3")
+        git clone https://github.com/cmangos/mangos-cata.git $scriptpath/mangos
+        git clone https://github.com/ACID-Scripts/CATA.git $scriptpath/acid
+        git clone https://github.com/UDB-434/Database.git $scriptpath/db
+      ;;
+      esac
+    fi
 
     read -p "Do you want to intall a boost package [y or n] ? " bool
     if test "$bool" == "y"
@@ -115,31 +115,77 @@ case "$action" in
       sudo apt-get install libboost-all-dev
     fi
 
-    # Build the thing
-    mkdir $scriptpath/build
-       cd $scriptpath/build
-    cmake ../mangos
+    read -p "Do you want to build the source now [y or n] ? " bool
+    if test "$bool" == "y"
+    then
+      sudo rm -rf $scriptpath/build
+        mkdir $scriptpath/build
+           cd $scriptpath/build
+        cmake ../mangos -DCMAKE_INSTALL_PREFIX=$scriptpath/run -DPCH=1 -DDEBUG=0
+         make
+         make install
+    fi
+
+    cp $scriptpath/mangos/src/mangosd/mangosd.conf.dist.in $scriptpath/run/mangosd.conf
+    cp $scriptpath/mangos/src/realmd/realmd.conf.dist.in $scriptpath/run/realmd.conf
+    cp $scriptpath/mangos/src/game/AuctionHouseBot/ahbot.conf.dist.in $scriptpath/run/ahbot.conf
+
+    read -p "Do you want to create empty databases [y or n] ? " bool
+    if test "$bool" == "y"
+    then
+      mysql -uroot -p < mangos/sql/create/db_create_mysql.sql
+    fi
+
+    read -p "Do you want to initialize databases [y or n] ? " bool
+    if test "$bool" == "y"
+    then
+      mysql -uroot -p mangos < mangos/sql/base/mangos.sql
+      mysql -uroot -p characters < mangos/sql/base/characters.sql
+      mysql -uroot -p realmd < mangos/sql/base/realmd.sql
+    fi
+
+    read -p "Do you want to populate the database [y or n] ? " bool
+    if test "$bool" == "y"
+    then
+      case "$idtitle" in
+      "0")
+      ;;
+      "1")
+      ;;
+      "2")
+          cd $scriptpath/db/
+        sudo rm -f InstallFullDB.config
+        sudo chmod 777 InstallFullDB.sh
+          sh InstallFullDB.sh
+        echo "Please replace the following line for CORE_PATH"
+        echo "CORE_PATH=$scriptpath/mangos"
+        sudo gedit InstallFullDB.config
+          sh InstallFullDB.sh
+      ;;
+      "3")
+      ;;
+      esac
+    fi
 
   ;;
-  "remove")
-    echo "Removing package ..."
-    sudo apt-get remove $srvname
-    sudo update-rc.d -f $srvname remove
-    sudo rm /etc/init.d/$srvname
-    sudo rm -r $scriptpath/$config
+  "purgemysql")
+    echo "This will purge the mysql package like it was just installed"
+    read -p "Do you want to continue with this process [y or n] ? " bool
+    if test "$bool" == "y"
+    then
+      sudo apt-get purge mysql-server mysql-client mysql-common mysql-server-core-5.5 mysql-client-core-5.5
+      sudo rm -rf /etc/mysql /var/lib/mysql
+      sudo apt-get autoremove
+      sudo apt-get autoclean
+    fi
   ;;
   "config")
-    echo "Opening settings ..."
-    sudo gedit $scriptpath/$config/minidlna.conf
   ;;
   "stats")
     echo "Home: $HOME"
     echo "PWDD: $PWD"
     echo "Name: $scriptname"
     echo "Path: $scriptpath"
-    echo "SRVN: $srvname"
-    echo "Conf: $config"
-    echo "Proj: $projdir"
   ;;
   *)
     echo "Usage: $0 { update | install | remove | config | stats }"
