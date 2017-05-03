5e03a5ec868cd798f071eb1316b56109b8871ac6
Deyan Dobromirov <dvd_video@abv.bg>
2017-05-03 23:14:32 +0300

    Added: Running

--- SteamCMD/config.sh
+++ SteamCMD/config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-action="$1"
+SCMDACTION="$1"
 
-user=""
-pass=""
-appid=""
-appmod=""
-bool=""
-chkhash=""
-archit=""
-datedt=$(date +"%y-%m-%d_%H-%M-%S")
-scriptname=$(readlink -f "$0")
-scriptpath=$(dirname "$scriptname")
+SCMDUSER=""
+SCMDPASS=""
+SCMDAPPID=""
+SCMDAPPMOD=""
+SCMDBOOL=""
+SCMDCHKHASH=""
+SCMDARCHIT=""
+SCMDDATEDT=$(date +"%y-%m-%d_%H-%M-%S")
+SCMDPRGNAME=$(readlink -f "$0")
+SCMDPRGPATH=$(dirname "$SCMDPRGNAME")
 
 function getInput()
 {
@@ -30,27 +30,27 @@ function getAppModID()
 
   echo "$1"
 
-  getInput "Enter an appid to install" "appid" rid
+  getInput "Select appid to $SCMDACTION" "appid" rid
 
   if test "$rid" == "90"
   then # https://developer.valvesoftware.com/wiki/Dedicated_Servers_List (4020)
-    getInput "Select a mod for HL1 / CS1.6 [n or <mod_name>]" "a mod" rmd
-  fi
-
+    read -p "Select a mod for HL1 / CS1.6 [mod_name]: " rmd
+  fi # Hor HL1 appid=90 mod_name is empty
+  
   eval "$2=$rmd"
   eval "$3=$rid"
 }
 
 echo "This script manages SteamCMD dedicated server"
 
-case "$action" in
+case "$SCMDACTION" in
   "update")
-    read -p "Do you want to install dependencies ? [y or n] " bool
-    if test "$bool" = "y"
+    read -p "Do you want to install dependencies [y/N] ? " SCMDBOOL
+    if test "$SCMDBOOL" == "y"
     then
-      archit=$(uname -m)
-      echo "Your linux kernel core is : $archit"
-      if test "$archit" == "x86_64"
+      SCMDARCHIT=$(uname -m)
+      echo "Your linux kernel core is : $SCMDARCHIT"
+      if test "$SCMDARCHIT" == "x86_64"
       then
         dpkg --add-architecture i386
         apt-get update
@@ -61,22 +61,22 @@ case "$action" in
       fi
     fi
 
-    echo "Refreshing directory /steamcmd at <$scriptpath>"
+    echo "Refreshing directory /steam at <$SCMDPRGPATH>"
 
-    cd $scriptpath
-    mkdir steamcmd
-    cd steamcmd
+    cd $SCMDPRGPATH
+    mkdir steam
+    cd steam
     rm -f steamcmd*.*
 
-    echo "Downloading steam ..."
+    echo "Downloading Steam ..."
     wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 
-    chkhash=$(md5sum steamcmd_linux.tar.gz | cut -d' ' -f1)
-    if test "$chkhash" == "09e3f75c1ab5a501945c8c8b10c7f50e"
+    SCMDCHKHASH=$(md5sum steamcmd_linux.tar.gz | cut -d' ' -f1)
+    if test "$SCMDCHKHASH" == "09e3f75c1ab5a501945c8c8b10c7f50e"
     then
       echo "Checksum OK"
     else
-      echo "Checksum FAIL $chkhash"
+      echo "Checksum FAIL $SCMDCHKHASH"
       exit 0
     fi
 
@@ -84,45 +84,72 @@ case "$action" in
 
     chmod +x steamcmd.sh
 
-    read -p "Do you wish to login now ? [y or n] " bool
-    if test "$bool" == "y"
+    read -p "Do you wish to login now [y/N] ? " SCMDBOOL
+    if test "$SCMDBOOL" == "y"
     then
-      getInput "Enter a user for steam, or login as anonymous" "user name" user
+      read -p "Write a steam user, or press [Enter] for anonymous" SCMDUSER
+      if [ -z "$SCMDUSER" ]
+      then
+        SCMDUSER="anonymous"
+      fi
     else
       echo "Running steam update check"
       ./steamcmd.sh +quit
       exit 0
     fi
 
-    getAppModID "Select desired server application to be installed" appmod appid
+    getAppModID "Select server application to be installed" SCMDAPPMOD SCMDAPPID
 
-    if test "$user" == "anonymous"
+    echo "Application: <$SCMDUSER> <$SCMDAPPID> <$SCMDAPPMOD>"
+
+    if test "$SCMDUSER" == "anonymous"
     then
-      if test "$appmod" == "n"
+      if [ -z "$SCMDAPPMOD" ]
       then
-        ./steamcmd.sh +login $user +app_update $appid validate +quit
+        ./steamcmd.sh +login $SCMDUSER +force_install_dir ../$SCMDAPPID +app_update $SCMDAPPID validate +quit
       else
-        ./steamcmd.sh +login $user +app_update $appid validate +app_set_config "90 mod $appmod" +quit
+        ./steamcmd.sh +login $SCMDUSER +force_install_dir ../$SCMDAPPID/$SCMDAPPMOD +app_update $SCMDAPPID validate +app_set_config "90 mod $SCMDAPPMOD" +quit
       fi
     else
-      getInput "Enter the password for user [$user]" "a password" pass
-      if test "$appmod" == "n"
+      getInput "Enter the password for user [$SCMDUSER]" "a password" SCMDPASS
+      if [ -z "$SCMDAPPMOD" ]
       then
-        ./steamcmd.sh +login $user $pass +app_update $appid validate +quit
+        ./steamcmd.sh +login $SCMDUSER $SCMDPASS +force_install_dir ../$SCMDAPPID +app_update $SCMDAPPID validate +quit
       else
-        ./steamcmd.sh +login $user $pass +app_update $appid validate +app_set_config "90 mod $appmod" +quit
+        ./steamcmd.sh +login $SCMDUSER $SCMDPASS +force_install_dir ../$SCMDAPPID/$SCMDAPPMOD +app_update $SCMDAPPID validate +app_set_config "90 mod $SCMDAPPMOD" +quit
       fi
     fi
   ;;
   "backup")
   ;;
   "stats")
-    echo "Date: $datedt"
-    echo "Path: $scriptpath"
-    echo "Name: $scriptname"
+    echo "Date: $SCMDDATEDT"
+    echo "Path: $SCMDPRGPATH"
+    echo "Name: $SCMDPRGNAME"
+  ;;
+  "run")
+    SCMDARCHIT=$(uname -m)
+    getAppModID "Select the server you wish to start" SCMDAPPMOD SCMDAPPID
+    if test "$SCMDAPPID" == "90"
+    then
+      if [ -z "$SCMDAPPMOD" ]
+      then
+        echo "Please select a game to start for HLDS !"
+        exit 0
+      fi
+      # /home/deyan/.steam/sdk32/steamclient.so
+      # with error:
+      # /home/deyan/.steam/sdk32/steamclient.so: cannot open shared object file: No such
+      # Clien fix (root): sudo cp ../../steamclient.so_32 steamclient.so
+      # Clien fix (root): sudo cp ../../steamclient.so_90_64 steamclient.so
+      # netstat -antuwp | egrep "(^[^t])|(*LISTEN)"
+      cd $SCMDPRGPATH/$SCMDAPPID
+      ./hlds_run -game $SCMDAPPMOD +maxplayers $2 +map $3 +port $4
+    else
+      ./srcds_run -game $SCMDAPPMOD +maxplayers $2 +map $3 +port $4
+    fi
   ;;
   *)
-    echo "Usage: $0 { update | backup | stats }"
+    echo "Usage: $0 { update | run | backup | stats }"
   ;;
 esac
-
