111c51809c18316dc7d443ecbc1bea91d62d5cad
dvdvideo1234 <dvd_video@abv.bg>
2019-05-17 17:14:31 +0300

    Updated: Apt-Get lock file fix

--- General/remLockAPT.sh
+++ General/remLockAPT.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+ARDIR=(
+/var/lib/dpkg/lock
+/var/lib/apt/lists/lock
+/var/lib/dpkg/lock-frontend
+/var/cache/apt/archives/lock
+)
+
+PID=""
+
 if (( $EUID != 0 )); then
   echo "The script must be run as root !"
   exit 0
 fi
 
-lsof /var/lib/dpkg/lock | kill
-lsof /var/lib/apt/lists/lock | kill
-lsof /var/lib/dpkg/lock-frontend | kill
-lsof /var/cache/apt/archives/lock | kill
-
-rm /var/lib/dpkg/lock
-rm /var/lib/apt/lists/lock
-rm /var/lib/dpkg/lock-frontend
-rm /var/cache/apt/archives/lock
+for LINE in ${ARDIR[@]}
+do
+  PID=$(lsof $LINE)
+  echo [$PID] $LINE
+  rm -f $LINE
+done
 
 dpkg --configure -a
-
-apt-get update
 
 exit 0
 
