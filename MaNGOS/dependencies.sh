761339f43aefcb1d3f6b2eaa35114beb349ecd96
dvdvideo1234 <dvd_video@abv.bg>
2019-08-24 10:13:28 +0300

    Added: MaNGOS dependencies

---
+++ MaNGOS/dependencies.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+apt-get update
+# Dependencies
+
+apt-get install build-essential
+apt-get install gcc
+apt-get install g++
+apt-get install automake
+apt-get install git-core
+apt-get install autoconf
+apt-get install make
+apt-get install patch
+apt-get install libmysql++-dev
+apt-get install mysql-server
+apt-get install libtool
+apt-get install libssl-dev
+apt-get install grep
+apt-get install binutils
+apt-get install zlibc
+apt-get install libc6
+apt-get install libbz2-dev
+apt-get install cmake
+apt-get install subversion
+apt-get install libboost-all-dev
+
+exit 0
