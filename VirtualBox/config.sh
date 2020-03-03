27f2e57167e2fcc53acb3e4d277ba2c6a24b540a
Deyan Dobromirov <dvd_video@abv.bg>
2020-03-03 11:06:14 +0200

    Updated: VB preparation

--- VirtualBox/config.sh
+++ VirtualBox/config.sh
@@ -2,13 +2,14 @@
 
 bool=""
 
-echo "This script is designed to ge run before"
+echo "This script is designed to be run before"
 echo "inserting the Guest Addition CD in Ubuntu VirtualBox system."
-read -p "Do you want to install this software [y/n]? : " bool
+echo "Your system: ${uname -a}"
+read -p "Do you want to install this software [y/N]? : " bool
 
 if test "$bool" == "y"
 then
-  read -p "Do you want to install dependancies [y/n]? : " bool
+  read -p "Do you want to install dependencies [y/N]? : " bool
   if test "$bool" == "y"
   then
     apt-get update
@@ -18,13 +19,36 @@ then
     apt-get install virtualbox-ose-guest-x11
     apt-get install virtualbox-guest-dkms 
     apt-get install linux-signed-generic
+    
+    read -p "Do you want to run package-update [y/N]? : " bool
+    if test "$bool" == "y"
+    then
+      echo "The following packages have upgrade option ..."
+      apt list --upgradable
+      
+      read -p "Start package upgrade [y/N]? : " bool
+      if test "$bool" == "y"
+      then
+        echo "Upgrading package list ..."
+        apt upgrade
+      fi  
+      
+      read -p "Reboot the system [y/N]? : " bool
+      if test "$bool" == "y"
+      then
+        echo "Rebooting ..."
+        reboot
+      fi  
+    fi  
   fi
-  read -p "Do you want to run force-update [y/n]? : " bool
+  
+  read -p "Do you want to run force-update [y/N]? : " bool
   if test "$bool" == "y"
   then
     update-manager -d
     echo "Click the RESTART button in the update dialog box"
   fi
+  
   echo "Proceed with installing Guest Additions CD .."
 fi
 
