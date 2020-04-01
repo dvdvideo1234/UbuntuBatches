ad3e83cceaee3ef5b921dda84b1c8cc38f9cdb43
Deyan Dobromirov <dvd_video@abv.bg>
2020-04-01 13:22:34 +0300

    Fixed: Use `chrome-gnome-shell` dependency to fix transparent tweak tool theme

--- General/config.sh
+++ General/config.sh
@@ -5,6 +5,9 @@ if (( $EUID != 0 )); then
   exit 0
 fi
 
+# Refresh PPAs
+sudo apt-get autoremove && sudo apt-get autoclean
+
 # Install GParted
 wget -q -O- http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
 sh -c 'echo "deb http://archive.getdeb.net/ubuntu trusty-getdeb apps" >> /etc/apt/sources.list'
@@ -23,6 +26,14 @@ apt-get install samba
 apt-get install samba-common
 apt-get install python-glade2
 apt-get install system-config-samba
+
+# Install Gnome tweak tool
+sudo apt install chrome-gnome-shell
+sudo apt-get install gnome-tweak-tool
+
+# Install Unity tweak tool
+sudo apt-get install unity-webapps-common unity-tweak-tool
+sudo apt-get install unity-tweak-tool
 
 exit 0
 
