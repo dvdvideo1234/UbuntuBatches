7ffba8d61763402910ffda70263e47f51610bfdf
Deyan Dobromirov <dvd_video@abv.bg>
2020-04-22 21:09:38 +0300

    Added: More user control on the server ID selected

--- SteamCMD/lgsm-steamcmd.sh
+++ SteamCMD/lgsm-steamcmd.sh
@@ -1,36 +1,51 @@
 #!/bin/bash
 
-base="Documents/LinuxGSM/"
-svid=""
+svid=0
 svop=""
 bool="Y"
 array=()
 
 # Change directory to LinuxGSM root
-
 cd ~
-cd ${base}
+cd Documents/LinuxGSM/
 
 # Gather list of executables with no spaces in the name
-
+# Count trough the list and add them to the array
 for file in $(find . -maxdepth 1 -type f ! -name "*.*" -type f ! -name "* *" -perm -og+rx)
 do
   array+=("$file")
 done
 
-# Count trough the list above and add them to the array
+# Use arithmetic to obtain highest index
+maxid=$((${#array[*]} - 1))
 
-count=${#array[*]}
+while [ 0 -eq 0 ]; do 
+  # Keep cycling until proper server ID in range is selected
+  echo "Servers available : [$(($maxid + 1))]"
 
-echo "Servers available : [$count]"
+  # List all the server executables
+  for ((i = 0 ; i <= $maxid ; i++)); do
+    echo "[$i] : ${array[i]}"
+  done
 
-for ((i = 0 ; i <= ($count-1) ; i++)); do
-  echo "[$i] : ${array[i]}"
-done
-
-# Select desired server to star it via ID
-
-read -p "Enter server ID: " svid
+  # Select desired server to start via ID
+  read -p "Enter server ID: " svid 
+  svid=$(echo "$svid" | grep -E "^[+-]?[0-9]+$")
+  
+  # Check for empty value
+  if [ -z "$svid" ]
+  then
+    echo "Empty value is not correct!"
+  else
+    # Check the server range is between 0 and maxid
+    if [ $svid -ge 0 -a $svid -le $maxid ]
+    then
+      break
+    else
+      echo "Server index [$svid] not in range[0, $maxid]"
+    fi
+  fi
+done 
 
 # Set server title as user can have many servers
 
