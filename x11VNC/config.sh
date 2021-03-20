681710d11f34d78f9d34fe9c8ab1cef28669c239
Deyan Dobromirov <dvd_video@abv.bg>
2021-03-20 14:58:10 +0200

    Fixed: XDisplay resolution to 1360x768 Added: x11VNC/config.sh installs and configures dummy VD

--- x11VNC/config.sh
+++ x11VNC/config.sh
@@ -2,10 +2,12 @@
 
 action="$1"
 param="$2"
+passw=""
 srvname="x11vnc"
 scriptname=$(readlink -f "$0")
 scriptpath=$(dirname "$scriptname")
 configloc="/etc/init"
+configxdp="/etc/X11"
 
 function getInput()
 {
@@ -20,14 +22,20 @@ function getInput()
 case "$action" in
   "install")
     echo "Installing package ... $srvname"
+        
+    # Install dependencies
     sudo apt-get update 
     sudo apt-get install vim
+    sudo apt-get install xserver-xorg-video-dummy
     sudo apt-get install $srvname
+    sudo cp xorg.conf $configxdp/xorg.conf
     
+    # Create runner configurator for the port provided
     eval "$scriptname run $param"
-        
-    getInput "Enter installation password: " param
-    $srvname -storepasswd $param $scriptpath/$srvname.pass
+    
+    # Create password for the server must not be empty
+    getInput "Enter installation password: " passw
+    $srvname -storepasswd $passw $scriptpath/$srvname.pass
   ;;
   "run")
     echo "Scripting package ... $srvname"
@@ -40,13 +48,26 @@ case "$action" in
     echo "Removing package ... $srvname"
     sudo /usr/bin/killall $srvname
     sudo apt-get remove $srvname
-    sudo mv $configloc/$srvname.conf $scriptpath/$srvname.conf
+    sudo apt-get remove xserver-xorg-video-dummy
+    
+    # Remove server configuration
+    sudo mv $configloc/$srvname.conf $scriptpath/$srvname.conf.bak
     sudo rm -f $configloc/$srvname.conf
+    
+    # Remove xdisplay configuration
+    sudo mv $configxdp/xorg.conf $scriptpath/xorg.conf.bak
+    sudo rm -f $configxdp/xorg.conf
+    
+    # Remove password not needed anymore
     rm -f $scriptpath/$srvname.pass
   ;;
   "config")
     echo "Configuring package ... $srvname"
     sudo vim $configloc/$srvname.conf
+  ;;
+  "xsetup")
+    echo "Configuring XDisplay ... $srvname"
+    sudo vim $configxdp/xorg.conf
   ;;
   "start")
     echo "Starting package ... $srvname"
@@ -62,9 +83,10 @@ case "$action" in
     echo "Scrn: $scriptname"
     echo "Path: $scriptpath"
     echo "EtcC: $configloc/$srvname.conf"
+    echo "XDsp: $configxdp/xorg.conf"
   ;;
   *)
-    echo "Usage: $0 { install <port> | remove | config | start <port> | stop | stats }"
+    echo "Usage: $0 { install <port> | remove | config | xsetup | start <port> | stop | stats }"
   ;;
 esac
 
