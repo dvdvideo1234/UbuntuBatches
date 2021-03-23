02eb241dbd285c4eb623d1791b3b5f1af66a37ab
Deyan Dobromirov <dvd_video@abv.bg>
2021-03-23 20:45:18 +0200

    Added: Samba troubleshooting for NETBIOS

--- General/config.sh
+++ General/config.sh
@@ -58,6 +58,19 @@ apt-get install psensor
 #    force user     = <user>
 #    valid users    = <user>
 #    guest ok       = yes
+# When Icon is not shown in the windows network
+# netbios name     = PC_%h
+# name resolve order = lmhosts host wins bcast
+# cat /proc/sys/net/ipv6/conf/all/disable_ipv6
+# sudo sysctl -p
+# net.ipv6.conf.all.disable_ipv6 = 1
+# net.ipv6.conf.default.disable_ipv6 = 1
+# net.ipv6.conf.lo.disable_ipv6 = 1
+# sudo service nmbd start
+# sudo systemctl restart nmbd
+# Add the samva IP to hosts
+# cat /etc/hosts
+# 127.0.0.1       localhost
 apt-get install cifs-utils
 apt-get install python3-samba
 apt-get install samba-common-bin
